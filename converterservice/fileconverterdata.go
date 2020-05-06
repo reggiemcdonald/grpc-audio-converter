27a1b4b216f90bb2865ce4812545dd0dfad85d0b
reggiemcdonald <regmcdonald95@gmail.com>
2020-05-05 21:41:48 -0600

    Refactor to dependency injection
    This commit begins adding dependency injection to the converterservice to make it easier to test its components.

--- converterservice/fileconverterdata.go
+++ converterservice/fileconverterdata.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-type FileConverterDataService interface {
+type FileConverterDataRepository interface {
 	NewRequest(id string) (bool, error)
 	CompleteConversion(id string, url string) (bool, error)
 	FailConversion(id string) (bool, error)
@@ -21,10 +21,10 @@ type FileConverterData struct {
 }
 
 type ConvertJob struct {
-	id          string
-	status      string
-	currUrl     string
-	lastUpdated time.Time
+	Id          string
+	Status      string
+	CurrUrl     string
+	LastUpdated time.Time
 }
 
 /*
@@ -53,8 +53,8 @@ func NewFileConverterData(dbUser string, dbPass string) *FileConverterData {
 /*
  * Inserts a request into the database
  * SCHEMA:
- *   id string PRIMARY_KEY
- *   status string [QUEUED | CONVERTING | COMPLETED | FAILED]
+ *   Id string PRIMARY_KEY
+ *   Status string [QUEUED | CONVERTING | COMPLETED | FAILED]
  *   curr_url string
  *   last_updated timestamp
  */
@@ -69,10 +69,10 @@ func (f *FileConverterData) NewRequest(id string) (bool, error) {
 }
 
 /*
- * Updates the status of the current file conversion to complete, including the presigned URL to the bucket object
+ * Updates the Status of the current file conversion to complete, including the presigned URL to the bucket object
  */
 func (f *FileConverterData) CompleteConversion(id string, url string) (bool, error) {
-	stmt := fmt.Sprintf("UPDATE %s SET status=$1, curr_url=$2, last_updated=$3 WHERE id=$4", tableName)
+	stmt := fmt.Sprintf("UPDATE %s SET Status=$1, curr_url=$2, last_updated=$3 WHERE Id=$4", tableName)
 	status, lastUpdated := pb.ConvertFileQueryResponse_COMPLETED.String(), time.Now()
 	_, err := f.db.Exec(stmt, status, url, lastUpdated, id)
 	if err != nil {
@@ -82,10 +82,10 @@ func (f *FileConverterData) CompleteConversion(id string, url string) (bool, err
 }
 
 /*
- * Updates the status of the specified file conversion to failed, along with the timestamp of failure
+ * Updates the Status of the specified file conversion to failed, along with the timestamp of failure
  */
 func (f *FileConverterData) FailConversion(id string) (bool, error) {
-	stmt := fmt.Sprintf("UPDATE %s SET status=$1, last_updated=$2 WHERE id=$3", tableName)
+	stmt := fmt.Sprintf("UPDATE %s SET Status=$1, last_updated=$2 WHERE Id=$3", tableName)
 	status, lastUpdated := pb.ConvertFileQueryResponse_FAILED.String(), time.Now()
 	_, err := f.db.Exec(stmt, status, lastUpdated, id)
 	if err != nil {
@@ -98,7 +98,7 @@ func (f *FileConverterData) FailConversion(id string) (bool, error) {
  * Fetches ConvertJob from the database
  */
 func (f *FileConverterData) GetConversion(id string) (*ConvertJob, error) {
-	stmt := fmt.Sprintf("SELECT * FROM %s WHERE id=$1", tableName)
+	stmt := fmt.Sprintf("SELECT * FROM %s WHERE Id=$1", tableName)
 	var (
 		status string
 		currUrl string
---
+++ converterservice/mocks/mockfileconverterdata.go
@@ -0,0 +1,77 @@
+// Mocks FileConverterData
+package mocks
+
+import (
+	"errors"
+	"fmt"
+	"github.com/reggiemcdonald/grpc-audio-converter/converterservice"
+	"github.com/reggiemcdonald/grpc-audio-converter/pb"
+	"time"
+)
+
+type MockFileConverterDb struct {
+	repo map[string]*converterservice.ConvertJob
+	success bool
+}
+
+func NewMockFileConverterDb() *MockFileConverterDb {
+	return &MockFileConverterDb{
+		repo: make(map[string]*converterservice.ConvertJob),
+		success: true,
+	}
+}
+
+/*
+ * Allows the user to fail any calls to the mock
+ */
+func (m *MockFileConverterDb) SetSuccess(success bool) {
+	m.success = success
+}
+
+/*
+ * Retrieves the data that is stored in the mock
+ */
+func (m *MockFileConverterDb) Repo() map[string]*converterservice.ConvertJob {
+	return m.repo
+}
+
+func (m *MockFileConverterDb) NewRequest(id string) (bool, error) {
+	if m.success {
+		m.repo[id] = &converterservice.ConvertJob{
+			Id: id,
+			Status: pb.ConvertFileQueryResponse_CONVERTING.String(),
+			CurrUrl: "NONE",
+			LastUpdated: time.Now(),
+		}
+		return true, nil
+	}
+	return false, errors.New(fmt.Sprintf("failed to create request %s", id))
+}
+
+func (m *MockFileConverterDb) CompleteConversion(id string, url string) (bool, error) {
+	if m.success && m.repo[id] != nil {
+		job := m.repo[id]
+		job.CurrUrl = url
+		job.Status = pb.ConvertFileQueryResponse_COMPLETED.String()
+		job.LastUpdated = time.Now()
+		return true, nil
+	}
+	return false, errors.New(fmt.Sprintf("failed to set completion in DB for id %s and url %s", id, url))
+}
+
+func (m *MockFileConverterDb) FailConversion(id string) (bool, error) {
+	if m.success && m.repo[id] != nil {
+		job := m.repo[id]
+		job.Status = pb.ConvertFileQueryResponse_FAILED.String()
+		job.LastUpdated = time.Now()
+		return true, nil
+	}
+	return false, errors.New(fmt.Sprintf("failed to set failure in DB for Id %s", id))
+}
+
+func (m *MockFileConverterDb) GetConversion(id string) (*converterservice.ConvertJob, error) {
+	if m.success && m.repo[id] != nil {
+		return m.repo[id], nil
+	}
+	return nil, errors.New(fmt.Sprintf("could not get job by id %s", id))
+}
