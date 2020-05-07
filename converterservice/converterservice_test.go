2d7dced902cc1806c0dfb8708363677bb1ec626b
reggiemcdonald <regmcdonald95@gmail.com>
2020-05-07 15:36:47 -0600

    add tests
    This commit adds test cases for the converterservice.

---
+++ converterservice/converterservice_test.go
@@ -0,0 +1,134 @@
+// Tests the ConverterServer
+package converterservice_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/reggiemcdonald/grpc-audio-converter/converterservice"
+	"github.com/reggiemcdonald/grpc-audio-converter/converterservice/mocks"
+	"github.com/reggiemcdonald/grpc-audio-converter/pb"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+var testGrpcRequest = &pb.ConvertFileRequest{
+	SourceUrl: "test-url",
+	SourceEncoding: pb.Encoding_MP3,
+	DestEncoding: pb.Encoding_WAV,
+}
+
+type testServerConfiguration struct {
+	Port int
+	S3service *mocks.S3ServiceMock
+	Db *mocks.MockFileConverterDb
+	ExecutableFactory *mocks.MockExecutableFactory
+}
+
+func testingConfiguration() *testServerConfiguration {
+	port := 3000
+	s3Service := mocks.NewMockS3Service(testRegion, testS3Endpoint, testBucketName)
+	db := mocks.NewMockFileConverterDb()
+	return &testServerConfiguration{
+		Port: port,
+		S3service: s3Service,
+		Db: db,
+		ExecutableFactory: mocks.NewMockExecutableFactory(),
+	}
+}
+
+func toServerConfiguration(testConfig *testServerConfiguration) *converterservice.ConverterServerConfig {
+	return &converterservice.ConverterServerConfig{
+		Db: testConfig.Db,
+		ExecutableFactory: testConfig.ExecutableFactory,
+		Port: testConfig.Port,
+		S3service: testConfig.S3service,
+	}
+}
+
+func TestNewWithConfiguration(t *testing.T) {
+	config := toServerConfiguration(testingConfiguration())
+	t.Run("config.ExecutableFactory=nil", func(t *testing.T) {
+		newConfig := &converterservice.ConverterServerConfig{
+			Port: config.Port,
+			S3service: config.S3service,
+			Db: config.Db,
+		}
+		server := converterservice.NewWithConfiguration(newConfig)
+		assert.NotNil(t, server, "server should not be nil")
+	})
+	t.Run("config.ExecutableFactory=mockExecutableFactory", func(t *testing.T) {
+		server := converterservice.NewWithConfiguration(config)
+		assert.NotNil(t, server, "server should not be nil")
+	})
+}
+
+func TestConverterServer_ConvertFile(t *testing.T) {
+	config := testingConfiguration()
+	server := converterservice.NewWithConfiguration(toServerConfiguration(config))
+	res, err := server.ConvertFile(context.TODO(), testGrpcRequest)
+	assert.Nil(t, err, "should not have errored")
+	assert.NotNil(t, res, "response should not be nil")
+	assert.NotNil(t, res.Id, "response should have an ID")
+	assert.True(t, res.Accepted, "request should have been accepted")
+	job, err := config.Db.GetConversion(res.Id)
+	assert.Nil(t, err, "there should not be an error with getting result")
+	assert.NotNil(t, job, "job should not be nil")
+	assert.Equal(t, res.Id, job.Id, "job should be the right job")
+}
+
+func TestConverterServer_ConvertFileQuery_Success(t *testing.T) {
+	// timeout strategy from https://stackoverflow.com/questions/24929790/how-to-set-the-go-timeout-flag-on-go-test
+	timeout := time.After(3 * time.Second)
+	done := make(chan bool)
+	config := testingConfiguration()
+	server := converterservice.NewWithConfiguration(toServerConfiguration(config))
+	res, err := server.ConvertFile(context.TODO(), testGrpcRequest)
+	assert.Nil(t, err, "should not have errored")
+	assert.NotNil(t, res, "response should not be nil")
+	job, err := config.Db.GetConversion(res.Id)
+	go func() {
+		for job.Status != pb.ConvertFileQueryResponse_COMPLETED.String() &&
+			job.Status != pb.ConvertFileQueryResponse_FAILED.String() {}
+		done <- true
+	}()
+	select {
+	case <-timeout:
+		t.Fatal("timeout waiting for DB status change")
+	case <-done:
+	}
+	assert.Equal(t, pb.ConvertFileQueryResponse_COMPLETED.String(), job.Status, "test should have successfully executed")
+	assert.Equal(t, fmt.Sprintf("http://%s.%s/%s/%s", testRegion, testS3Endpoint, testBucketName, res.Id),
+		job.CurrUrl, "should have a properly formatted URL")
+	assert.GreaterOrEqual(t, time.Now().Unix(), job.LastUpdated.Unix(), "last updated should be recent")
+}
+
+func TestConverterServer_ConvertFileQuery_Fail(t *testing.T) {
+	// timeout strategy from https://stackoverflow.com/questions/24929790/how-to-set-the-go-timeout-flag-on-go-test
+	timeout := time.After(3 * time.Second)
+	done := make(chan bool)
+	config := testingConfiguration()
+	server := converterservice.NewWithConfiguration(toServerConfiguration(config))
+	config.S3service.Success = false
+	res, err := server.ConvertFile(context.TODO(), testGrpcRequest)
+	assert.Nil(t, err, "should not have errored")
+	assert.NotNil(t, res, "response should not be nil")
+	job, err := config.Db.GetConversion(res.Id)
+	go func() {
+		for job.Status != pb.ConvertFileQueryResponse_COMPLETED.String() &&
+			job.Status != pb.ConvertFileQueryResponse_FAILED.String() {}
+		done <- true
+	}()
+	select {
+	case <-timeout:
+		t.Fatal("timeout on test case")
+	case <-done:
+	}
+	assert.Equal(t, pb.ConvertFileQueryResponse_FAILED.String(), job.Status, "test should have failed to execute")
+	assert.Equal(t, "NONE", job.CurrUrl, "URL should be none")
+	assert.GreaterOrEqual(t, time.Now().Unix(), job.LastUpdated.Unix(), "last updated should be recent")
+}
+
+func TestConverterServer_ConvertStream(t *testing.T) {
+	t.Skip("not implemented")
+}
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/lib/pq v1.3.0
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/stretchr/testify v1.5.1
 	github.com/youpy/go-riff v0.0.0-20131220112943-557d78c11efb // indirect
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
 	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
